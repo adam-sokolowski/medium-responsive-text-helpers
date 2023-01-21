8346c18942c491e5504235624f08b73d2a62905d
Adam Sokołowski <adam.sokolowski@europcar.com>
2023-01-21 09:07:22 +0100

    feat(all): adds storybook with sass and themes support

---
+++ stories/Typography.js
@@ -0,0 +1,9 @@
+import { html } from 'lit-html';
+
+export const TypographyPage = () => html`
+  <article>
+    <section>
+    ...
+    </section>
+  </article>
+`;
---
+++ stories/Typography.stories.js
@@ -0,0 +1,9 @@
+import { TypographyPage } from './Typography';
+
+export default {
+  title: 'Typography/Helpers',
+};
+
+const Typography = (args) => TypographyPage(args);
+
+export const Helpers = Typography.bind({});
